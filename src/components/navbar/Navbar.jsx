b86ebd5f3c048792f4467f80363546f2f44241fe
KipkorirA <aron.kipkorir@student.moringaschool.com>
2024-11-15 15:10:49 +0300

    done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
             <div className="absolute inset-0 w-full h-full z-[-1] opacity-70 bg-[url('/images/Navbar-background.png')] bg-cover bg-center bg-no-repeat mix-blend-overlay" />
 
             <div className="flex-1 z-20 px-6"> 
-                <Link to="/" className="block hover:opacity-80 transition-all duration-300 transform hover:scale-105 -mb-20 -mt-20">
+                <Link to="/" className="block hover:opacity-80 transition-all duration-300 transform hover:scale-105 lg:-mb-20 lg:-mt-20">
                     <img src="/images/logo.png" alt="CozyCare Logo" className="w-[350px] h-auto lg:w-[450px] drop-shadow-lg" />
                 </Link>
             </div>
