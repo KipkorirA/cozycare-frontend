d84ec5dc4c0a32b07abbf7fb1407e55e185a0f9a
KipkorirA <aron.kipkorir@student.moringaschool.com>
2024-11-23 21:36:51 +0300

    Done

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -26,7 +26,7 @@ const ContactForm = () => {
         if (isCaptchaValid) {
             setIsLoading(true);
             try {
-                const response = await fetch('http://127.0.0.1:5000/contacts', {
+                const response = await fetch('https://cozycare-backend-g56w.onrender.com/contacts', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
